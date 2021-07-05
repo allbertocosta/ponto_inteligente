a114a2c82623431a8daeeaed49793d3a26d87afd
Alberto Costa <mihcadevs@gmail.com>
2021-07-04 23:13:50 -0300

    Implementando classe de teste lancamento repository

--- src/test/java/com/costa/pontointeligente/repositories/LancamentoRepositoryTest.java
+++ src/test/java/com/costa/pontointeligente/repositories/LancamentoRepositoryTest.java
@@ -1,5 +1,97 @@
 package com.costa.pontointeligente.repositories;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.costa.pontointeligente.entities.Empresa;
+import com.costa.pontointeligente.entities.Funcionario;
+import com.costa.pontointeligente.entities.Lancamento;
+import com.costa.pontointeligente.enums.PerfilEnum;
+import com.costa.pontointeligente.enums.TipoEnum;
+import com.costa.pontointeligente.utils.PasswordUtils;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
 public class LancamentoRepositoryTest {
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private Long funcionarioId;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		
+		Funcionario funcionario = this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+		this.funcionarioId = funcionario.getId();
+		
+		this.lancamentoRepository.save(obterDadosLancamentos(funcionario));
+		this.lancamentoRepository.save(obterDadosLancamentos(funcionario));
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.empresaRepository.deleteAll();
+	}
+
+	@Test
+	public void testBuscarLancamentosPorFuncionarioId() {
+		List<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId);
+		
+	Assertions.assertEquals(2, lancamentos.size());
+	}
+	
+	@Test
+	public void testBuscarLancamentosPorFuncionarioIdPaginado() {
+		PageRequest page = PageRequest.of(0, 10);
+		Page<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId, page);
+		
+		Assertions.assertEquals(2, lancamentos.getTotalElements());
+	}
+	
+	private Lancamento obterDadosLancamentos(Funcionario funcionario) {
+		Lancamento lancameto = new Lancamento();
+		lancameto.setData(new Date());
+		lancameto.setTipo(TipoEnum.INICIO_ALMOCO);
+		lancameto.setFuncionario(funcionario);
+		return lancameto;
+	}
+
+	private Funcionario obterDadosFuncionario(Empresa empresa) throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Fulano de Tal");
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
+		funcionario.setCpf("24291173474");
+		funcionario.setEmail("email@email.com");
+		funcionario.setEmpresa(empresa);
+		return funcionario;
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj("51463645000100");
+		return empresa;
+	}
 
 }
